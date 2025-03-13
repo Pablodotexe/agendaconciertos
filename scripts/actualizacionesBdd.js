b2bb8b3dde3339e878c96b4ac9b46060e77f8ec3
Pablodotexe <73339137+Pablodotexe@users.noreply.github.com>
2025-03-13 17:48:16 +0100

    Update actualizacionesBdd.js

--- scripts/actualizacionesBdd.js
+++ scripts/actualizacionesBdd.js
@@ -9,51 +9,25 @@ let addHora = document.getElementById("addHora").value;
 let addCartel = document.getElementById("addCartel").value;
 
 console.log(addSala);
-
-//console.log({ id, sala, fecha, hora, cartel });
-
     let xhr = new XMLHttpRequest();
-    //xhr.open("POST", `../php/addConciertos.php?nombreBanda=${nombreBanda}&genero=${genero}&ciudad=${ciudad}&fecha=${fecha}&hora=${hora}&cartel=${cartel}`, true);
-    
     xhr.open("POST", "../php/addConciertos.php", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             let respuesta = xhr.responseText;
-            console.log(respuesta);
+            //console.log(respuesta);
+            alert(respuesta);
         }
     };
     let data = `addBanda=${encodeURIComponent(addBanda)}&addSala=${encodeURIComponent(addSala)}
     &addCiudad=${encodeURIComponent(addCiudad)}&addGenero=${encodeURIComponent(addGenero)}
-    &addFecha=${encodeURIComponent(addFecha)}&addHora=${encodeURIComponent(addHora)}&addCartel=${encodeURIComponent(addCartel)}`;
+    &addFecha=${encodeURIComponent(addFecha)}&addHora=${encodeURIComponent(addHora)}
+    &addCartel=${encodeURIComponent(addCartel)}`;
     xhr.send(data);
 }
 
 
-function editarConcierto(){
-let editId = document.getElementById("editId").value;
-let editFecha = document.getElementById("editFecha").value;
-let editHora = document.getElementById("editHora").value;
-let editCartel = document.getElementById("editCartel").value;
-let xhr = new XMLHttpRequest();
-    //xhr.open("POST", `../php/editarConcierto.php?id=${id}&fecha=${fecha}&hora=${hora}&cartel=${cartel}`, true);
-    xhr.open("POST", "../php/editarConcierto.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            let respuesta = xhr.responseText;
-            console.log(respuesta);
-            /*if(respuesta===true){
-                alert("El concierto ha sido editado");
-            }else{
-                alert("Se ha producido un error. Por favor, vuelva a intentarlo");
-            }*/
-        }
-    };
-    let data = `editId=${encodeURIComponent(editId)}&editFecha=${encodeURIComponent(editFecha)}&editHora=${encodeURIComponent(editHora)}&editCartel=${encodeURIComponent(editCartel)}`;
-    xhr.send(data);
 
-}
 
 
 function loadConcerts() {
@@ -65,6 +39,7 @@ function loadConcerts() {
                 let listaConciertos = JSON.parse(xhr.responseText);
                 console.log(listaConciertos);  // Para depuración
                 mostrarConciertosBorrado(listaConciertos);
+                mostrarConciertosEditar(listaConciertos)
             } catch (error) {
                 console.error("Error al parsear JSON:", error, xhr.responseText);
             }
@@ -74,6 +49,45 @@ function loadConcerts() {
     xhr.send();
 }
 
+function mostrarConciertosEditar(listaConciertos) {
+
+    let divSelect = document.getElementById("editId");
+    
+        for (let i = 0; i < listaConciertos.length; i++) {
+            for (let j = 0; j < listaConciertos[i].concierto.length; j++) {
+                let contador = 1;
+                let option = document.createElement("option");
+                option.value = listaConciertos[i].concierto[j].id; // Convertimos el objeto a string;
+                option.textContent = listaConciertos[i].concierto[j].banda_nombre
+                + " - " + listaConciertos[i].concierto[j].sala_nombre
+                + " - " + listaConciertos[i].concierto[j].fecha_concierto
+                + " - " + listaConciertos[i].concierto[j].hora;
+    
+                divSelect.appendChild(option);
+                contador++;
+            }
+        }
+    }
+
+function editarConcierto(){
+    let editId = document.getElementById("editId").value;
+    let editFecha = document.getElementById("editFecha").value;
+    let editHora = document.getElementById("editHora").value;
+    let editCartel = document.getElementById("editCartel").value;
+    let xhr = new XMLHttpRequest();
+        xhr.open("POST", "../php/editarConcierto.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                let respuesta = xhr.responseText;
+                //console.log(respuesta);
+                alert(respuesta);
+            }
+        };
+        let data = `editId=${encodeURIComponent(editId)}&editFecha=${encodeURIComponent(editFecha)}&editHora=${encodeURIComponent(editHora)}&editCartel=${encodeURIComponent(editCartel)}`;
+        xhr.send(data);
+    
+    }
 
 // Función para borrar el concierto seleccionado
 function mostrarConciertosBorrado(listaConciertos) {
@@ -84,12 +98,9 @@ let divSelect = document.getElementById("borrarConcierto");
         for (let j = 0; j < listaConciertos[i].concierto.length; j++) {
             let contador = 1;
             let option = document.createElement("option");
-            option.value = listaConciertos[i].concierto[j].banda_id; // Convertimos el objeto a string;
+            option.value = listaConciertos[i].concierto[j].id; // Convertimos el objeto a string;
             option.textContent = listaConciertos[i].concierto[j].banda_nombre
-            /*+ " - " + listaConciertos[i].concierto[j].id
-            + " - " + listaConciertos[i].concierto[j].banda_id
-            + " - " + listaConciertos[i].concierto[j].sala_id*/
-            + " - " + listaConciertos[i].concierto[j].ciudad
+            + " - " + listaConciertos[i].concierto[j].sala_nombre
             + " - " + listaConciertos[i].concierto[j].fecha_concierto
             + " - " + listaConciertos[i].concierto[j].hora;
 
@@ -99,18 +110,17 @@ let divSelect = document.getElementById("borrarConcierto");
     }
 }
 
-function borrarConcierto(){
+function borrarConciertos(){
     let idConcierto = document.getElementById("borrarConcierto").value;
     console.log(idConcierto)
     let xhr = new XMLHttpRequest();
-    //xhr.open("POST", `../php/editarConcierto.php?id=${id}&fecha=${fecha}&hora=${hora}&cartel=${cartel}`, true);
     xhr.open("POST", "../php/borrarConcierto.php", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             let respuesta = xhr.responseText;
-            console.log(respuesta);
-            
+            //console.log(respuesta);
+            alert(respuesta);
         }
     }
     let data = `idConcierto=${encodeURIComponent(idConcierto)}`
