2764eb1559114380e826e25bd43af6fe4baf729a
Pablodotexe <73339137+Pablodotexe@users.noreply.github.com>
2025-03-15 09:36:03 +0100

    Update conciertos.php

--- paginas/conciertos.php
+++ paginas/conciertos.php
@@ -16,23 +16,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Próximos Conciertos</title>
+    <title>Agenda Conciertos</title>
     <link rel="stylesheet" href="../styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </head>
-<body>
+
+<!--Asociamos el id del body al usuario para recoger luego su ID en el PHP y poder usarlo
+posteriormente en la actualización de la tabla ASISTENCIAS-->
+<body data-user-id="<?= htmlspecialchars($_SESSION['nombre']) ?>">
     
 <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="../index.php">Agenda</a>
+        <a class="navbar-brand" href="../index.php"><img src="../imagenes/guit1.png" style="width:75px" alt="Agenda"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+                <a class="nav-link" aria-current="page" href="../index.php">Home</a>
                 <a class="nav-link" href="registro.php">Registrarse</a>
                 <a class="nav-link" href="../index.php">Iniciar sesión</a>
                 <a class="nav-link" href="conciertos.php">Consultar agenda</a>
@@ -40,7 +43,7 @@
                 
                 <?php if (isset($_SESSION['nombre'])): ?>
                     <!-- Mostrar botón Cerrar Sesión si hay un usuario logueado -->
-                    <a id="botonCerrarSesion" class="nav-link btn btn-danger text-white ms-3" href="../php/logout.php">Cerrar Sesión (<?= htmlspecialchars($_SESSION['nombre']) ?>)</a>
+                    <a href="../php/logout.php"><button id="botonCerrarSesion">Cerrar Sesión (<?= htmlspecialchars($_SESSION['nombre']) ?>)</button></a>
                 <?php endif; ?>
             </div>
         </div>
@@ -65,10 +68,10 @@
                 <div class="form-group">
                     <label for="genre">Estilo Musical:</label>
                         <select id="genre" name="genre">
-                            <option value="metal">metal</option>
-                            <option value="rock">rock</option>
-                            <option value="pop">pop</option>
-                            <option value="rap">rap</option>
+                            <option value="metal">METAL</option>
+                            <option value="rock">ROCK</option>
+                            <option value="pop">POP</option>
+                            <option value="rap">RAP</option>
                         </select>
                 </div>
 
