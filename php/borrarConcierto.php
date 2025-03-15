a56b74377313b1e3ad86b5b42b32c4b27f101e87
Pablodotexe <73339137+Pablodotexe@users.noreply.github.com>
2025-03-15 09:32:10 +0100

    Update borrarConcierto.php

--- php/borrarConcierto.php
+++ php/borrarConcierto.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "aaaa", "conciertos");
+$conn = new mysqli("sql112.infinityfree.com", "if0_37790823", "26G5hrP31G", "if0_37790823_conciertos");
 
 if ($conn->connect_error) {
     die("Error al establecer la conexión: " . $conn->connect_error);
@@ -7,6 +7,12 @@ if ($conn->connect_error) {
 
 // Recogemos el ID del concierto desde el formulario
 $idConcierto = $_POST["idConcierto"];
+
+// Validamos que se ha proporcionado elegido un concierto en el select
+if (!is_numeric($idConcierto)) {
+    echo "Debe seleccionar un concierto";
+    die();
+}
 
 // Comprobamos que el ID no esté vacío
 if (!empty($idConcierto)) {
