30b4eca9a54d63058a9f52971c91aae73db5821e
Pablodotexe <73339137+Pablodotexe@users.noreply.github.com>
2025-03-15 09:33:56 +0100

    Update mostrarConciertos.php

--- php/mostrarConciertos.php
+++ php/mostrarConciertos.php
@@ -4,12 +4,13 @@ header("Content-Type: application/json; charset=UTF-8");
 
 
 
-$conn = new mysqli("localhost", "root", "aaaa", "conciertos");
+$conn = new mysqli("sql112.infinityfree.com", "if0_37790823", "26G5hrP31G", "if0_37790823_conciertos");
 
 
 if ($conn->connect_error) {
     die("Error al conectar con la BD: " . $conn->connect_error);
 }
+
 
 $arrayConciertos = array();
 $fecha_inicio = $_GET['fecha_inicio'];
@@ -18,25 +19,22 @@ $genero = $_GET['genero'];
 $ciudad = $_GET['ciudad'];
 
 
-$sql = "SELECT 
-conciertos.fecha_concierto,
-conciertos.hora,
-bandas.nombre AS banda_nombre,
-conciertos.cartel AS banda_imagen,  /* NUEVA COLUMNA DE IMAGEN */
-salas.nombre AS sala_nombre,
-ciudades.nombre AS ciudad_nombre  /* NUEVA COLUMNA PARA EL NOMBRE DE LA CIUDAD */
-FROM
-conciertos
-JOIN 
-bandas ON conciertos.banda_id = bandas.id
-JOIN 
-salas ON conciertos.sala_id = salas.id
-JOIN 
-ciudades ON salas.ciudad_id = ciudades.id
-WHERE 
-conciertos.fecha_concierto BETWEEN '$fecha_inicio' AND '$fecha_fin'
-AND bandas.genero = '$genero'
-AND ciudades.nombre = '$ciudad'";
+$sql = "SELECT conciertos.id,
+               conciertos.fecha_concierto,
+               conciertos.hora,
+               bandas.nombre AS banda_nombre,
+               conciertos.cartel AS banda_imagen,  /* NUEVA COLUMNA DE IMAGEN */
+               salas.nombre AS sala_nombre,
+               ciudades.nombre AS ciudad_nombre  /* NUEVA COLUMNA PARA EL NOMBRE DE LA CIUDAD */
+        FROM conciertos
+        JOIN bandas ON conciertos.banda_id = bandas.id
+        JOIN salas ON conciertos.sala_id = salas.id
+        JOIN ciudades ON salas.ciudad_id = ciudades.id
+        WHERE conciertos.fecha_concierto BETWEEN '$fecha_inicio' AND '$fecha_fin'
+          AND bandas.genero = '$genero'
+          AND ciudades.nombre = '$ciudad'
+        ORDER BY conciertos.fecha_concierto ASC";
+
 
 
 $result = $conn->query($sql);
@@ -53,6 +51,7 @@ class Conciertos{
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
 
+        $idConcierto = $row["id"];
         $banda = $row['banda_nombre'];
         $sala = $row['sala_nombre'];
         $fecha_concierto = $row['fecha_concierto'];
@@ -61,6 +60,7 @@ if ($result->num_rows > 0) {
         
         $datos = array(
             array(
+                "id_concierto"=>$idConcierto,
                 "banda_nombre" => $banda,
                 "sala_nombre" => $sala,
                 "fecha_concierto" => $fecha_concierto,
@@ -75,78 +75,6 @@ if ($result->num_rows > 0) {
 $conn->close();
 $jsonConciertos = json_encode($arrayConciertos);
 echo $jsonConciertos;
-
-
-/*$conn = new mysqli("localhost", "root", "aaaa", "conciertos");
-
-
-if ($conn->connect_error) {
-    die("Error al conectar con la BD: " . $conn->connect_error);
-}
-
-$arrayConciertos = array();
-$fecha_inicio = $_GET['fecha_inicio'];
-$fecha_fin = $_GET['fecha_fin'];
-$genero = $_GET['genero'];
-$ciudad = $_GET['ciudad'];
-
-$sql = "SELECT 
-            conciertos.fecha_concierto,
-            conciertos.hora,
-            bandas.nombre AS banda_nombre,
-            salas.nombre AS sala_nombre
-        FROM
-            conciertos
-        JOIN 
-            bandas ON conciertos.banda_id = bandas.id
-        JOIN 
-            salas ON conciertos.sala_id = salas.id
-        JOIN 
-            ciudades ON salas.ciudad_id = ciudades.id
-        WHERE 
-            conciertos.fecha_concierto BETWEEN '$fecha_inicio' AND '$fecha_fin'
-            AND bandas.genero = '$genero'
-            AND ciudades.nombre = '$ciudad'";
-
-
-
-$result = $conn->query($sql);
-
-class Conciertos
-{
-    public $datos = array();
-
-    public function __construct($datos)
-    {
-        $this->datos = $datos;
-    }
-}
-
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-
-        $banda = $row['banda_nombre'];
-        $sala = $row['sala_nombre'];
-        $fecha_concierto = $row['fecha_concierto'];
-
-
-        $datos = array(
-            array(
-                "banda_nombre" => $banda,
-                "sala_nombre" => $sala,
-                "fecha_concierto" => $fecha_concierto
-            )
-        );
-        $arrayConciertos[] = new Conciertos($datos);
-    }
-}
-
-$conn->close();
-$jsonConciertos = json_encode($arrayConciertos);
-echo $jsonConciertos;
-
-*/
-
 
 
 ?>
